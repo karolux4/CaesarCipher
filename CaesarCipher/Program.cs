f76ef5bacfa06717ce89de49faec8d45c5c97bb9
karolux4 <karolisbutkus4@gmail.com>
2021-09-13 20:38:15 +0300

    Added console IO
    Added console interface to use cipher

--- CaesarCipher/Program.cs
+++ CaesarCipher/Program.cs
@@ -8,10 +8,45 @@ namespace CaesarCipher
     {
         static void Main()
         {
-            Cipher c = new Cipher(3);
-            
-            Console.WriteLine(c.Encrypt("ABc"));
-            Console.WriteLine(c.Decrypt("DeF"));
+            int n, mode;
+            string output = null;
+
+            Console.WriteLine("Welcome, please enter shift number (classic Caeser uses 3): ");
+            while (!Int32.TryParse(Console.ReadLine(), out n ))
+            {
+                Console.WriteLine("Enter valid number");
+            }
+
+            Console.WriteLine("Enter mode number: [1] Encrypt, [2] Decrypt");
+            while (!Int32.TryParse(Console.ReadLine(), out mode)&&(mode!=1||mode!=2))
+            {
+                Console.WriteLine("Enter valid number");
+            }
+
+            Console.WriteLine("Enter text: ");
+            while (output == null)
+            {
+                string input = Console.ReadLine();
+                Cipher c = new Cipher(n);
+                switch (mode)
+                {
+                    case 1:
+                        output = c.Encrypt(input);
+                        break;
+                    case 2:
+                        output = c.Decrypt(input);
+                        break;
+                }
+                if (output == null)
+                {
+                    Console.WriteLine("Text can only contain latin letters ");
+                }
+                else
+                {
+                    Console.WriteLine("Result: " + output);
+                }
+            }
+
         }
     }
 }
